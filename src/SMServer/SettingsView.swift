02744932800926a0d410d16c28dd77a11a96285e
iandwelker <iandwelker@gmail.com>
2020-07-07 22:59:02 -0600

    Allows for texts without interaction

--- src/SMServer/SettingsView.swift
+++ src/SMServer/SettingsView.swift
@@ -9,18 +9,59 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State var port: String = UserDefaults.standard.object(forKey: "port") as? String ?? "8741"
+    @State var password: String = UserDefaults.standard.object(forKey: "password") as? String ?? "toor"
+    
     @State var default_num_chats = UserDefaults.standard.object(forKey: "num_chats") as? Int ?? 40
     @State var default_num_messages = UserDefaults.standard.object(forKey: "num_messages") as? Int ?? 100
     @State var server_ping = UserDefaults.standard.object(forKey: "server_ping") as? Int ?? 60
     
     @State var debug: Bool = UserDefaults.standard.object(forKey: "debug") as? Bool ?? false
     @State var start_on_load: Bool = UserDefaults.standard.object(forKey: "start_on_load") as? Bool ?? false
-    
-    @State var port: String = UserDefaults.standard.object(forKey: "port") as? String ?? "8741"
-    @State var password: String = UserDefaults.standard.object(forKey: "password") as? String ?? "toor"
     @State var require_authentication: Bool = UserDefaults.standard.object(forKey: "require_auth") as? Bool ?? true
     
     var body: some View {
+        
+        let port_binding = Binding<String>(get: {
+            self.port
+        }, set: {
+            var possible_port = $0.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+            if possible_port.count < 4 {
+                possible_port = UserDefaults.standard.object(forKey: "port") as? String ?? "8741"
+            }
+            self.port = possible_port
+            UserDefaults.standard.setValue(possible_port, forKey: "port")
+        })
+        
+        let pass_binding = Binding<String>(get: {
+            self.password
+        }, set: {
+            self.password = $0
+            UserDefaults.standard.setValue($0, forKey: "password")
+        })
+        
+        let chats_binding = Binding<Int>(get: {
+            self.default_num_chats
+        }, set: {
+            self.default_num_chats = Int($0)
+            UserDefaults.standard.setValue(Int($0), forKey: "num_chats")
+        })
+        
+        let messages_binding = Binding<Int>(get: {
+            self.default_num_messages
+        }, set: {
+            self.default_num_messages = Int($0)
+            UserDefaults.standard.setValue(Int($0), forKey: "num_messages")
+        })
+        
+        let ping_binding = Binding<Int>(get: {
+            self.server_ping
+        }, set: {
+            self.server_ping = Int($0)
+            self.debug ? print("setting val for ping: \($0)") : nil
+            UserDefaults.standard.setValue(Int($0), forKey: "server_ping")
+        })
+        
         let debug_binding = Binding<Bool>(get: {
             self.debug
         }, set: {
@@ -35,34 +76,6 @@ struct SettingsView: View {
             UserDefaults.standard.setValue($0, forKey: "start_on_load")
         })
         
-        let pass_binding = Binding<String>(get: {
-            self.password
-        }, set: {
-            self.password = $0
-            UserDefaults.standard.setValue($0, forKey: "password")
-        })
-        
-        let port_binding = Binding<String>(get: {
-            self.port
-        }, set: {
-            self.port = $0
-            UserDefaults.standard.setValue($0, forKey: "port")
-        })
-        
-        let chats_binding = Binding<Int>(get: {
-            self.default_num_chats
-        }, set: {
-            self.default_num_chats = $0
-            UserDefaults.standard.setValue($0, forKey: "num_chats")
-        })
-        
-        let messages_binding = Binding<Int>(get: {
-            self.default_num_messages
-        }, set: {
-            self.default_num_messages = $0
-            UserDefaults.standard.setValue($0, forKey: "num_messages")
-        })
-        
         let auth_binding = Binding<Bool>(get: {
             self.require_authentication
         }, set: {
@@ -70,42 +83,37 @@ struct SettingsView: View {
             UserDefaults.standard.setValue($0, forKey: "require_auth")
         })
         
-        let ping_binding = Binding<Int>(get: {
-            self.server_ping
-        }, set: {
-            self.server_ping = $0
-            UserDefaults.standard.setValue($0, forKey: "server_ping")
-        })
-        
         return VStack(spacing: 20) {
             HStack {
-                Text("Settings").font(.title)
+                Text("Settings")
+                    .font(.title)
                 Spacer()
             }
             
             HStack {
-                Text("Change default port").font(.subheadline)
+                Text("Change default port")
+                    .font(.subheadline)
                 Spacer()
             }
             
-            TextField("Change default server port", text: port_binding)
+            TextField("Change default port (value must be >= 1000)", text: port_binding)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-                .keyboardType(.numberPad)
             
             HStack {
-                Text("Change password").font(.subheadline)
+                Text("Change password")
+                    .font(.subheadline)
                 Spacer()
             }
             
             TextField("Change requests password", text: pass_binding)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
+                .disableAutocorrection(true)
             
             HStack {
                 Text("Initial number of chats to load")
                 Spacer()
                 TextField("Chats", value: chats_binding, formatter: NumberFormatter())
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .keyboardType(.numberPad)
                     .frame(width: 60)
             }
             
@@ -114,7 +122,6 @@ struct SettingsView: View {
                 Spacer()
                 TextField("Messages", value: messages_binding, formatter: NumberFormatter())
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .keyboardType(.numberPad)
                     .frame(width: 60)
             }
             
@@ -123,7 +130,6 @@ struct SettingsView: View {
                 Spacer()
                 TextField("Ping", value: ping_binding, formatter: NumberFormatter())
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .keyboardType(.numberPad)
                     .frame(width: 60)
             }
             
