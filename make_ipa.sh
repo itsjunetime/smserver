ddb696cc1a8bf10207e84c1a0054963513db4b94
iandwelker <iandwelker@gmail.com>
2020-07-08 17:32:11 -0600

    Updated ipa

--- make_ipa.sh
+++ make_ipa.sh
@@ -4,9 +4,10 @@ xcodebuild clean -workspace ./src/SMServer.xcworkspace -scheme SMServer
 xcodebuild build -workspace ./src/SMServer.xcworkspace -scheme SMServer
 xcodebuild archive -workspace ./src/SMServer.xcworkspace -scheme SMServer -archivePath ./ipa/SMServer.xcarchive CODE_SIGNING_ALLOWED=NO
 
-mkdir -p ipa/SMServer_new/Payload
-cp -r ipa/SMServer.xcarchive/Products/Applications/SMServer.app ipa/SMServer_new/Payload/SMServer.app
-zip ipa/SMServer.ipa ipa/SMServer_new
+mkdir -p ipa/Payload
+cp -r ipa/SMServer.xcarchive/Products/Applications/SMServer.app ipa/Payload/SMServer.app
+zip ipa/SMServer.zip ipa/Payload
+mv ipa/SMServer.zip ipa/SMServer.ipa
 
-rm -rf ipa/SMServer.xcarchive ipa/SMServer_new
+rm -rf ipa/SMServer.xcarchive ipa/Payload
 find . -name ".DS_Store" -delete
