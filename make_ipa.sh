c6cafe632e8fd0f9c59e39155f57a12ae8f785a1
iandwelker <iandwelker@gmail.com>
2020-07-08 22:36:00 -0600

    Fixed ipa sandbox issue (probably)

--- make_ipa.sh
+++ make_ipa.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-xcodebuild clean -workspace ./src/SMServer.xcworkspace -scheme SMServer
-xcodebuild build -workspace ./src/SMServer.xcworkspace -scheme SMServer
-xcodebuild archive -workspace ./src/SMServer.xcworkspace -scheme SMServer -archivePath ./ipa/SMServer.xcarchive CODE_SIGNING_ALLOWED=NO
+# xcodebuild clean -workspace ./src/SMServer.xcworkspace -scheme SMServer
+# xcodebuild build -workspace ./src/SMServer.xcworkspace -scheme SMServer
+# xcodebuild archive -workspace ./src/SMServer.xcworkspace -scheme SMServer -archivePath ./ipa/SMServer.xcarchive CODE_SIGNING_ALLOWED=NO
 
 mkdir -p ipa/Payload
-cp -r ipa/SMServer.xcarchive/Products/Applications/SMServer.app ipa/Payload/SMServer.app
-zip ipa/SMServer.zip ipa/Payload
+cp -r ipa/*.xcarchive/Products/Applications/SMServer.app ipa/Payload/SMServer.app
+codesign --entitlements src/entitlements.xml -f -s "Apple Development: votesforteens@gmail.com (S2597GTW8H)" ipa/Payload/SMServer.app
+ditto -c -k --sequesterRsrc --keepParent ipa/Payload ipa/SMServer.zip
+# open .
+# zip ipa/SMServer.zip ipa/Payload
 mv ipa/SMServer.zip ipa/SMServer.ipa
 
-rm -rf ipa/SMServer.xcarchive ipa/Payload
-find . -name ".DS_Store" -delete
+# rm -rf ipa/SMServer.xcarchive ipa/Payload
+# find . -name ".DS_Store" -delete
