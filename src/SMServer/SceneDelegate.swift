6bdb06c113fa1f49ac81593244283e5a552840ac
iandwelker <iandwelker@gmail.com>
2020-07-11 23:10:07 -0600

    Extended background life; extended compatibility; few minor fixes

--- src/SMServer/SceneDelegate.swift
+++ src/SMServer/SceneDelegate.swift
@@ -37,6 +37,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+        contentView.startBackgroundTask()
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
@@ -47,6 +48,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        contentView.startBackgroundTask()
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
@@ -58,6 +60,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        contentView.startBackgroundTask()
     }
 
 
