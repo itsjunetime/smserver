fafb3cfad91331b175214325fd3825a9a94b6bf0
iandwelker <iandwelker@gmail.com>
2020-07-15 11:51:23 -0600

    Updated UI, changed postinst, updated docs

--- src/SMServer/Obj.h
+++ src/SMServer/Obj.h
@@ -9,8 +9,19 @@
 #ifndef Obj_h
 #define Obj_h
 
-@interface sender : NSObject
+/*@interface IPCTextWatcher : NSObject
 
+@property (nonatomic, strong) void(^completion)(void);
+-(void)handleReceivedTextWithCallback;
+
+@end*/
+
+@interface Sender : NSObject
+
+//@property (nonatomic, strong) void(^call)(void);
+//@property (nonatomic, strong) IPCTextWatcher* watcher;
+
+//- (id)initWithCallback:(void(^)(void))call;
 - (void)launchMobileSMS;
 - (void)relaunchApp;
 - (uid_t)setUID;
