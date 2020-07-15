8472be91b6a0519fe3cec06f367d602784e8adfc
iandwelker <iandwelker@gmail.com>
2020-07-15 11:51:23 -0600

    Updated UI, changed postinst, updated docs

--- src/SMServer/Obj.m
+++ src/SMServer/Obj.m
@@ -11,7 +11,48 @@
 
 #import "Obj.h"
 
-@implementation sender
+/*@implementation IPCTextWatcher {
+    MRYIPCCenter* _center;
+}
+
++(void)load {
+    [self sharedInstance:nil];
+}
+
++(instancetype)sharedInstance:(void(^)(void))callback {
+    static dispatch_once_t onceToken = 0;
+    __strong static IPCTextWatcher* sharedInstance = nil;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[self alloc] init:callback];
+    });
+    return sharedInstance;
+}
+
+-(instancetype)init:(void(^)(void))callback {
+    if ((self = [super init])) {
+        _center = [MRYIPCCenter centerNamed:@"com.ianwelker.smserverHandleText"];
+        [_center addTarget:self action:@selector(handleReceivedTextWithCallback)];
+        _completion = callback;
+    }
+    return self;
+}
+
+-(void)handleReceivedTextWithCallback {
+    _completion();
+}
+
+@end*/
+
+@implementation Sender
+
+/*- (id)initWithCallback:(void(^)(void))call {
+    self = [super init];
+    if (self) {
+        _call = call;
+        _watcher = [IPCTextWatcher sharedInstance:_call];
+    }
+    return self;
+}*/
 
 - (void)relaunchApp {
     
@@ -39,9 +80,9 @@
 - (void)sendIPCText:(NSString *)body toAddress:(NSString *)address withAttachments:(NSArray *)paths {
     
     MRYIPCCenter* center = [MRYIPCCenter centerNamed:@"com.ianwelker.smserver"];
-    
     [center callExternalMethod:@selector(sendText:) withArguments:@{@"body": body, @"address": address, @"attachment": paths}];
     
 }
 
 @end
+
