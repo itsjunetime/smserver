0b12f51b652f5dc888613732bf5032101fef882f
iandwelker <iandwelker@gmail.com>
2020-07-17 23:39:55 -0600

    Fixed attachments/css issues

--- src/SMServer/SceneDelegate.swift
+++ src/SMServer/SceneDelegate.swift
@@ -46,7 +46,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-        contentView.endBackgroundTask()
+        //contentView.endBackgroundTask()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
@@ -61,7 +61,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
-        contentView.endBackgroundTask()
+        //contentView.endBackgroundTask()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
