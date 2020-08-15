ac9f9de80fed311a9eb08ea2a05cf112579b6244
iandwelker <iandwelker@gmail.com>
2020-08-14 23:58:35 -0600

    Cocoapods is broken but I did it all manually and now it compiles

--- src/Pods/Target Support Files/Pods-SMServer/Pods-SMServer-frameworks.sh
+++ src/Pods/Target Support Files/Pods-SMServer/Pods-SMServer-frameworks.sh
@@ -198,13 +198,13 @@ fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CocoaAsyncSocket/CocoaAsyncSocket.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/GCDWebServer/GCDWebServer.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Criollo/Criollo.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/HTTPParserC/HTTPParserC.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Telegraph/Telegraph.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CocoaAsyncSocket/CocoaAsyncSocket.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/GCDWebServer/GCDWebServer.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Criollo/Criollo.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/HTTPParserC/HTTPParserC.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Telegraph/Telegraph.framework"
 fi
