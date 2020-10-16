3f28f0e84129fdd8ff13a4eeb76c94391a68a1ba
iandwelker <iandwelker@gmail.com>
2020-10-15 23:57:26 -0600

    Massive changes; iOS 14+ support, have not yet tested iOS 13 support

--- src/SMServer/Obj.m
+++ src/SMServer/Obj.m
@@ -3,6 +3,8 @@
 
 #import "Obj.h"
 
+#define c(a) NSClassFromString(@#a)
+
 @implementation IPCTextWatcher {
     /// This is a MCRYIPC center that libsmserver contacts whenever a new text is received.
     MRYIPCCenter* _center;
@@ -21,25 +23,36 @@
     if ((self = [super init])) {
         _center = [MRYIPCCenter centerNamed:@"com.ianwelker.smserverHandleText"];
         [_center addTarget:self action:@selector(handleReceivedTextWithCallback:)];
-        [_center addTarget:self action:@selector(handleBatteryChanged)];
+        [_center addTarget:self action:@selector(handlePartyTypingWithCallback:)];
     }
     return self;
 }
 
 - (void)handleReceivedTextWithCallback:(NSString *)chat_id {
     /// This is the function that is called when a new text is received.
-    /// _setTexts is a block that is set somewhere around line 677 in ContentView.swift, in loadFuncs(). 
+    /// _setTexts is a block that is set somewhere around line 625 in ContentView.swift, in loadFuncs().
     _setTexts(chat_id);
+}
+
+- (void)handlePartyTypingWithCallback:(NSString *)chat_id {
+    /// This is called when someone else starts typing
+    /// _setTyping is a block that is set somewhere around line 629 in ContentView.swift, in loadFuncs().
+    _setTyping(chat_id);
 }
 
 @end
 
 @implementation IWSSender
 
+- (id)init {
+    if (self = [super init]) {
+        self.center = [MRYIPCCenter centerNamed:@"com.ianwelker.smserver"];
+    }
+    return self;
+}
+
 - (void)launchMobileSMS {
-    
-    MRYIPCCenter* center = [MRYIPCCenter centerNamed:@"com.ianwelker.smserverLaunch"];
-    [center callExternalVoidMethod:@selector(launchSMS) withArguments:nil];
+    [self.center callExternalVoidMethod:@selector(launchSMS) withArguments:nil];
 }
 
 - (uid_t)setUID {
@@ -50,22 +63,48 @@
 }
 
 - (void)sendIPCText:(NSString *)body withSubject:(NSString *)subject toAddress:(NSString *)address withAttachments:(NSArray *)paths {
-    
-    MRYIPCCenter* center = [MRYIPCCenter centerNamed:@"com.ianwelker.smserver"];
-    [center callExternalVoidMethod:@selector(sendText:) withArguments:@{@"body": body, @"subject": subject, @"address": address, @"attachment": paths}];
+    [self.center callExternalVoidMethod:@selector(sendText:) withArguments:@{@"body": body, @"subject": subject, @"address": address, @"attachment": paths}];
 }
 
 - (void)markConvoAsRead:(NSString *)chat_id {
-    
-    MRYIPCCenter* center = [MRYIPCCenter centerNamed:@"com.ianwelker.smserver"];
-    [center callExternalVoidMethod:@selector(setAllAsRead:) withArguments:chat_id];
+    [self.center callExternalVoidMethod:@selector(setAllAsRead:) withArguments:chat_id];
 }
 
 - (void)sendReaction:(NSNumber *)reaction forGuid:(NSString *)guid inChat:(NSString *)chat {
-    
-    MRYIPCCenter* center = [MRYIPCCenter centerNamed:@"com.ianwelker.smserver"];
-    [center callExternalVoidMethod:@selector(sendReaction:) withArguments:@{@"reaction": reaction, @"guid": guid, @"chat": chat}];
+    [self.center callExternalVoidMethod:@selector(sendReaction:) withArguments:@{@"reaction": reaction, @"guid": guid, @"chat": chat}];
 }
+
+- (void)sendTyping:(BOOL)isTyping forChat:(NSString *)chat {
+    NSBundle* imcore = [[NSBundle alloc] initWithPath:@"/System/Library/PrivateFrameworks/IMCore.framework"];
+    [imcore load];
+    
+    CKConversationList* sharedList = [c(CKConversationList) sharedConversationList];
+    CKConversation* convo = [sharedList conversationForExistingChatWithGroupID:chat];
+    
+    [convo setLocalUserIsTyping:isTyping];
+}
+
+/// This is not being used, but I am
+/*- (NSArray *)getPinnedChats {
+    NSBundle* imcore = [[NSBundle alloc] initWithPath:@"/System/Library/PrivateFrameworks/IMCore.framework"];
+    [imcore load];
+    
+    IMDaemonController* controller = [c(IMDaemonController) sharedController];
+    __block NSArray* pins = [NSArray array];
+    
+    if ([controller connectToDaemon]) {
+        [controller sendQueryWithReply:NO query:^{
+            if ([[NSProcessInfo processInfo] operatingSystemVersion].majorVersion >= 14) {
+                IMPinnedConversationsController* pinnedController = [c(IMPinnedConversationsController) sharedInstance];
+                NSOrderedSet* set = [pinnedController pinnedConversationIdentifierSet];
+                
+                pins = [set array];
+            }
+        }];
+    }
+    
+    return pins;
+}*/
 
 @end
 
